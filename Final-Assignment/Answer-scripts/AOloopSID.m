e79615995cf679c8f4d5a548da62ca08ef7b65f2
sjauhri <snehal.jauhri@gmail.com>
2019-01-17 15:24:33 +0100

    pushing Part3 initial

---
+++ Final-Assignment/Answer-scripts/AOloopSID.m
@@ -0,0 +1,67 @@
+function [var_eps] = AOloopSID(G,H, covariance_phi, sigma_e, phi_sim)
+% Variance calculation of an AO system in the closed-loop configuration
+% IN
+% G     : measurement matrix 
+% H     : influence matrix mapping the wavefront on the mirror
+% covariance_phi  : covariance matrix of the turbulence wavefront
+% sigma_eps : measurement noise parameter for determining its covariance
+% phi_sim : simulation data for the wavefront
+% OUT
+% var_eps : variance of the residual wavefront after taking N_t points
+% within the closed-loop operation
+
+% dimension lifted wavefront
+n_H = size(H,1);
+
+% dimension lifted sensor slopes
+n_G = size(G,1);
+
+% Number of sample points for phi_sim
+T = length(phi_sim);
+
+u = zeros(n_H,T);
+
+% This term is multiplied with the slope vector to obtain the eps_hat(k|k)
+% value
+eps_pred_multiplier_matrix = H\(covariance_phi*G'/(G*covariance_phi*G' + (sigma_e^2)*eye(n_G)));
+
+% epsilon matrix
+eps_k = zeros(size(phi_sim,1),T);
+
+% epsilon matrix with mean removed:
+eps_mean_removed_k = zeros(size(phi_sim,1),T);
+
+% Constructing a vector of all the variance values for eps
+var_eps = zeros(T,1);
+
+% An assumption is that H is full rank.
+
+% We have the data for phi_k, and we know the matrix H.
+% Since we don't have the values for the slopes s(k), we compute the vector as:
+% s(k) = G*phi_k - G*H*u(k-1) + sigma_e*randn(n_G,1)
+% We use this s(k) in the expression for the optimal increment u(k) -
+% u(k-1), and calculate the values of the u(k) vector recursively, since we
+% know u(0) = 0.
+% Based on the calculated u(k), we calculate the value of
+% epsilon and then accordingly its variance.
+
+% u(0) is 0 since we don't apply any control input before the first wavefront datum,
+% and hence we calculate u(1) taking u(0) = 0
+u(:,1) = eps_pred_multiplier_matrix*(G*phi_sim(:,1) + sigma_e*randn(n_G,1));
+eps_k(:,1) = phi_sim(:,1);
+
+eps_mean_removed_k(:,1) = eps_k(:,1) - mean(eps_k(:,1));
+
+var_eps(1) = var(eps_mean_removed_k(:,1));
+
+for k = 2:T
+    eps_k(:,k) = phi_sim(:,k) - H*u(:,k-1);
+    u(:,k) = eps_pred_multiplier_matrix*(G*eps_k(:,k) + sigma_e*randn(n_G,1)) + u(:,k-1);
+    eps_mean_removed_k(:,k) = eps_k(:,k) - mean(eps_k(:,k));
+    var_eps(k) = var(eps_mean_removed_k(:,k));
+end
+
+var_eps = mean(var_eps);
+
+end
+
---
+++ Final-Assignment/Answer-scripts/SubId.m
@@ -0,0 +1,79 @@
+function [As, Cs, Ks] = SubId(sid,s,n)
+% Variance calculation of an AO system in the closed-loop configuration
+% IN
+% sid : Slopes of Identification data
+% s   : Parameter 's'. Rows of Henkel matrix.
+% n   : Order of states 'n'
+% OUT
+% As : State Space matrix
+% Cs : State Space matrix
+% Ks : Kalman Gain in Observer form
+
+% Number of sample points for Subspace Identification
+N = size(sid,2);
+
+% Dimension of sid
+M = size(sid,1);
+
+% Construct Hankel Matrix using sid
+%Y = hankel(sid,1,s,N-s+1);
+Y = [];
+sid_stacked = sid(:);
+
+for i=1:N-s+1
+    Y = [Y sid_stacked(M*(i-1)+1:M*(s+i-1))];
+end
+
+Ypast = Y(:,1:N-2*s+1);
+Yfuture = Y(:,s+1:N-s+1);
+
+R = triu(qr([Ypast; Yfuture]'))';
+R32 = R(M*s+1:M*2*s,1:M*s);
+R22 = R(1:M*s,1:M*s);
+
+[U,Sigma,V] = svd(R32*inv(R22)*Ypast);
+
+% assignin('base','V',V);
+% assignin('base','R22',R22);
+% assignin('base','R32',R32);
+% assignin('base','U',U);
+% assignin('base','Sigma',Sigma);
+
+% Reduced System
+U = U(:,1:n);
+Sigma = Sigma(1:n,1:n);
+V = V(:,1:n);
+
+X = sqrt(Sigma)*V';
+
+% Solve least squares problem for X
+Xpast = X(:,1:end-1);
+Xfuture = X(:,2:end);
+Ylsq = sid(:,s+1:end-s);
+
+F = Xpast;
+Sys = (F*(F'))\(F*([Xfuture; Ylsq]'));
+Sys = Sys';
+
+As = Sys(1:n,:);
+Cs = Sys(n+1:end,:);
+
+% Find K using residuals
+Res = [Xfuture; Ylsq] - [As; Cs]*Xpast;
+Nt = size(Res,2);
+Covar = (Res*Res')/Nt;
+Q = Covar(1:n,1:n);
+S = Covar(1:n,n+1:end);
+R = Covar(n+1:end,n+1:end);
+
+% Choosing P from the solution of the DARE, we can define a Kalman gain Ks
+% that is asymptotically stable.
+% Solution of DARE:
+E = eye(5,5);
+[P,~,~] = dare(As',Cs',Q,R,S,E);
+
+% Kalman Gain:
+Ks = (S + As*P*(Cs'))*inv(R + (Cs*P*(Cs')));
+
+end
+
