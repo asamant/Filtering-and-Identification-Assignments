4300bb234a9b08412ad4c2cd71bb6666a2edcddd
asamant <43705966+asamant@users.noreply.github.com>
2019-01-18 11:43:12 +0100

    Committing changes to a temp branch to avoid conflicts
    - Update report
    - Update slope-minimizing RW files

--- Final-Assignment/Answer-scripts/residual_slope_RW.m
+++ Final-Assignment/Answer-scripts/residual_slope_RW.m
@@ -5,10 +5,12 @@ close all;
 load systemMatrices.mat; 
 load turbulenceData.mat; % load the data provided
 
-%[U, S, V] = svd(G*H, 0);
+%% Question 4.1
 
-[U_G, S_G, V_G] = svd(G);
+% Rank of G*H
+rank_GH = rank(G*H);
 
+% G*H is not full-rank; rank = 47
 [U, S, V] = svd(G*H);
 U1 = U(:,1:47);
 V1 = V(:,1:47);
@@ -53,22 +55,52 @@ var_s = zeros(T,1);
 
 for k = 2:T
     s_k(:,k) = G*(phi_sim(:,k) - H*u(:,k-1)) + sigmae*randn(n_G,1);
-    Y_k = G*eps_pred_multiplier_matrix*s_k(:,k) + G*H*u(:,k-1);
+    Y_k = G*(eps_pred_multiplier_matrix*s_k(:,k) + H*u(:,k-1));
     u(:,k) = (V1/Sigma_SVD)*U1'*Y_k;
     eps_k(:,k) = phi_sim(:,k) - H*u(:,k-1);
     
     var_s(k) = var(eps_k(:,k) - mean(eps_k(:,k)));
+    
 end
 
 var_s = mean(var_s);
+
+%% Question 4.2
+
+% VAF calculation
+
+deviation_norm_sum = 0;
+actual_phi_norm_sum = 0;
+
+for k = 2:T-1
+    % predicted_phi = eps_predicted + Hu(k)
+    predicted_phi = eps_pred_multiplier_matrix*s_k(:,k) + H*u(:,k-1);
+    predicted_phi = predicted_phi - mean(predicted_phi);
+    phi_sim_mean_removed = phi_sim(:,k+1) - mean(phi_sim(:,k+1));
+    current_norm = (norm(phi_sim_mean_removed - predicted_phi))^2;
+    deviation_norm_sum = deviation_norm_sum + current_norm;
+    actual_phi_norm_sum = actual_phi_norm_sum + (norm(phi_sim_mean_removed))^2;
+end
+    
+mean_deviation_norm = deviation_norm_sum/(T-2);
+mean_actual_norm = actual_phi_norm_sum/(T-2);
+VAF = max(0,100*(1 - mean_deviation_norm/mean_actual_norm));
 
 %% Question 4.3
 
 phi_vec = null(G);
 %phi_vec = phi_sim(:,1);
-phi_matrix = zeros(7,7);
+phi_matrix_1 = zeros(7,7);
+phi_matrix_2 = zeros(7,7);
 for i = 1:7
-    phi_matrix(:,i) = phi_vec(7*(i-1) + 1:7*i);
+    phi_matrix_1(:,i) = phi_vec(7*(i-1) + 1:7*i,1);
+    phi_matrix_2(:,i) = phi_vec(7*(i-1) + 1:7*i,2);
 end
 
-imagesc(phi_matrix)
+% To see how the turbulent wavefront needs to be to lie in the nullspace of
+% G
+figure(1);
+imagesc(phi_matrix_1);
+
+figure(2);
+imagesc(phi_matrix_2);
